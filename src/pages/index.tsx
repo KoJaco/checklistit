e95ead5e25facdc4546a8b9f879742ee791f3ee7
KoJaco <kojaco.dev@gmail.com>
2022-08-19 13:37:20 +1000

    build: implement dragDrop utils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,39 +5,11 @@ import { trpc } from '@/core/utils/trpc';
 import BaseLayout from '@/layouts/BaseLayout';
 import KanbanBoard from '@/components/kanban/KanbanBoard';
 
+import initialData from '@/static/ts/initialData';
+
 // const DynamicCheckList = dynamic(() => import('@/components/kanban/Column'), {
 //     ssr: false,
 // });
-
-const initialData = {
-    tasks: {
-        1: { id: 1, content: 'Configure Next.js application' },
-        2: { id: 2, content: 'Configure Next.js and tailwind ' },
-        3: { id: 3, content: 'Create sidebar navigation menu' },
-        4: { id: 4, content: 'Create page footer' },
-        5: { id: 5, content: 'Create page navigation menu' },
-        6: { id: 6, content: 'Create page layout' },
-    },
-    checkLists: {
-        'column-1': {
-            id: 'column-1',
-            title: 'TO-DO',
-            taskIds: [1, 2, 3, 4, 5, 6],
-        },
-        'column-2': {
-            id: 'column-2',
-            title: 'IN-PROGRESS',
-            taskIds: [],
-        },
-        'column-3': {
-            id: 'column-3',
-            title: 'COMPLETED',
-            taskIds: [],
-        },
-    },
-    // Facilitate reordering of the columns
-    columnOrder: ['column-1', 'column-2', 'column-3'],
-};
 
 const Home: NextPage = () => {
     return (
