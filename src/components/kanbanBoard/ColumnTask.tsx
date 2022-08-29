d23cef49b0876d565eb9542fd70a7e224fcfccea
KoJaco <kojaco.dev@gmail.com>
2022-08-29 11:16:24 +1000

    build: get textArea working with local state (saving content)

--- src/components/kanbanBoard/ColumnTask.tsx
+++ src/components/kanbanBoard/ColumnTask.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { AiOutlinePaperClip } from 'react-icons/ai';
 import ArrowIcon from '@/components/elements/ArrowIcon';
 
@@ -10,6 +10,8 @@ type ColumnTaskProps = {
 };
 
 const ColumnTask = ({ id, editing = false, ...props }: ColumnTaskProps) => {
+    // Controlled text area input, save button submits the form and affects DB + boardState
+    const [taskContent, setTaskContent] = useState<string>('');
     const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
     const handleTextAreaInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -19,8 +21,18 @@ const ColumnTask = ({ id, editing = false, ...props }: ColumnTaskProps) => {
         }
     };
 
+    function handleTaskContentChange(
+        event: React.ChangeEvent<HTMLTextAreaElement>
+    ) {
+        setTaskContent(event.currentTarget.value);
+    }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+
     return (
-        <form action="#" className="">
+        <form action="/" onSubmit={handleSubmit}>
             <div className="py-2 px-4 border border-gray-300 rounded-lg shadow-sm bg-gray-50 focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-slate-500 group h-full">
                 <label htmlFor="description" className="sr-only">
                     Description
@@ -42,33 +54,19 @@ const ColumnTask = ({ id, editing = false, ...props }: ColumnTaskProps) => {
                 <textarea
                     ref={textAreaRef}
                     rows={1}
-                    name="description"
-                    id="description"
+                    name="task"
+                    id="task"
                     className="border-0 w-full mb-2 resize-none no-scrollbar bg-gray-50 text-gray-500 focus:ring-0 sm:text-md focus:outline-none placeholder:italic placeholder:text-gray-500/[0.5] focus:resize-y"
                     placeholder="Start writing..."
+                    value={taskContent}
+                    onChange={handleTaskContentChange}
                     onInput={handleTextAreaInput}
-                    defaultValue={props.text}
+                    // defaultValue={taskContent}
                 />
-                {/* Spacer Element, show when editing or 'focused' */}
-
-                {/* <div aria-hidden="true">
-                    <div className="py-1">
-                        <div className="h-3" />
-                    </div>
-                    <div className="h-px" />
-                    <div className="py-1">
-                        <div className="py-px">
-                            <div className="h-3" />
-                        </div>
-                    </div>
-                </div> */}
 
                 <div className="opacity-0 max-h-0 border-t border-gray-200 flex justify-between items-center group-hover:opacity-100 group-hover:py-5 px-2 transition-all duration-1000 ease-in-out">
                     <div className="flex">
-                        <button
-                            type="button"
-                            className="-ml-2 -my-2 rounded-full py-2 inline-flex items-center text-left text-gray-400 group"
-                        >
+                        <button className="-ml-2 -my-2 rounded-full py-2 inline-flex items-center text-left text-gray-400 group">
                             <AiOutlinePaperClip
                                 className="h-5 w-5 mr-2 hover:text-gray-500"
                                 aria-hidden="true"
@@ -76,12 +74,11 @@ const ColumnTask = ({ id, editing = false, ...props }: ColumnTaskProps) => {
                         </button>
                     </div>
                     <div className="flex-shrink-0">
-                        <button
+                        <input
                             type="submit"
-                            className="inline-flex items-center px-3 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-primary-bg/[0.8] hover:bg-primary-bg hover:drop-shadow focus:outline-none focus:drop-shadow-lg transition-colors duration-1000"
-                        >
-                            Save
-                        </button>
+                            value="Save"
+                            className="inline-flex items-center px-3 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-primary-bg/[0.8] hover:bg-primary-bg hover:drop-shadow focus:outline-none focus:drop-shadow-lg transition-colors duration-1000 cursor-pointer"
+                        />
                     </div>
                 </div>
             </div>
